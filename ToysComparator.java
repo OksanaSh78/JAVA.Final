65af479916b5425bc52653c1a11ee6cbb4e4a3e4
user <oksi.1213@mail.ru>
2023-04-01 20:34:45 +1000

    Домашняя работа

---
+++ ToysComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class ToysComparator implements Comparator<ToysShop> {
+    @Override
+    public int compare(ToysShop t1, ToysShop t2) {
+        if (t1.getChance() < t2.getChance())
+            return 1;
+        if (t1.getChance() > t2.getChance())
+            return -1;
+        return 0;
+    }
+}
